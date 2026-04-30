af4e35b6228ace55735d292b411dc97f7f32fcae
Djura <d.smits@esciencecenter.nl>
2026-04-30 10:56:13 +0200

    Passing down shaker address and adapt unit tests

--- tests/test_teleshake1536.py
+++ tests/test_teleshake1536.py
@@ -32,6 +32,63 @@ class TeleShake1536_IO:
         self.output += rpl.Flatten()
 
 
+class TeleShake1536_IO_AddrCapture(TeleShake1536_IO):
+    def __init__(self) -> None:
+        super().__init__()
+        self.last_sent_addr: int = None
+
+    def write(self, msg: bytes):
+        frame = Frame.Unflatten(msg)
+        self.last_sent_addr = frame.Ctrl.addr
+        super().write(msg)
+
+
+class TestTeleshake1536Addr(unittest.TestCase):
+    def setUp(self) -> None:
+        self.serial = TeleShake1536_IO_AddrCapture()
+
+    def _assert_addr(self, addr):
+        self.assertEqual(self.serial.last_sent_addr, addr)
+
+    def test_SetRPM_addr(self):
+        self.serial.LoadedData.put_nowait([23, 28, 1])
+        with Teleshake1536(self.serial) as shaker:
+            shaker.SetRPM(5000, addr=3)
+        self._assert_addr(3)
+
+    def test_SetPower_addr(self):
+        self.serial.LoadedData.put_nowait([0x100 // 2, 0, 0])
+        with Teleshake1536(self.serial) as shaker:
+            shaker.SetPower(0.5, addr=2)
+        self._assert_addr(2)
+
+    def test_StartDevice_addr(self):
+        self.serial.LoadedData.put_nowait([0b000000010, 0, 0])
+        with Teleshake1536(self.serial) as shaker:
+            shaker.StartDevice(addr=1)
+        self._assert_addr(1)
+
+    def test_StopDevice_addr(self):
+        with Teleshake1536(self.serial) as shaker:
+            shaker.StopDevice(addr=2)
+        self._assert_addr(2)
+
+    def test_OpenClamp_addr(self):
+        with Teleshake1536(self.serial) as shaker:
+            shaker.OpenClamp(addr=1)
+        self._assert_addr(1)
+
+    def test_CloseClamp_addr(self):
+        with Teleshake1536(self.serial) as shaker:
+            shaker.CloseClamp(addr=3)
+        self._assert_addr(3)
+
+    def test_default_addr_is_zero(self):
+        with Teleshake1536(self.serial) as shaker:
+            shaker.StopDevice()
+        self._assert_addr(0)
+
+
 class TestTeleshake1536(unittest.TestCase):
     def setUp(self) -> None:
         self.serial = TeleShake1536_IO()
